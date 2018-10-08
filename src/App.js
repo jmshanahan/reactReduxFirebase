cd0d40415415f528ce82a0c38e7390a9fcebd017
jmshanahan <comeraghsolutions@gmail.com>
2018-10-08 14:30:34 +0100

    Added prettier eslint config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter} from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-            <div className="App">
-        <Navbar/>
-      </div>
+        <div className="App">
+          <Navbar />
+        </div>
       </BrowserRouter>
-
     );
   }
 }
